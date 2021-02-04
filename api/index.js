e5fbf701be2581334db4884d8c57d902dec9258d
Felipe Tun <felipe.tun.cauich@gmail.com>
2021-02-03 21:46:10 -0500

    estructura

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const sendFile = path.join(__dirname, '../dist/index.html')
+
+app.use('/public', express.static('../public'));
+app.set('port', port);
+
+// console.log(process.env.NODE_ENV)
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ foo: 'bar', bar: 'foo' });
+});
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(sendFile);
+    });
+} else {
+    const webpack = require('webpack');
+    const config = require('../webpack.config.js');
+    const compiler = webpack(config);
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const middleware = webpackMiddleware(compiler);
+
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    app.get('*', (req, res) => {
+        res.write(middleware.fileSystem.readFileSync(sendFile));
+        res.end();
+    });
+}
+
+app.listen(port, () => {
+  console.log(`Env: ${process.env.NODE_ENV} App listening at http://localhost:${port}`)
+})
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: {
+        main : path.join(__dirname, '/fe/index.js')
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js",
+        chunkFilename: '[name].bundle.js',
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        historyApiFallback: true,
+        port: 5000,
+        proxy: {
+            "/api": {
+                target : "http://localhost:" + process.env.PORT,
+                pathRewrite: {
+                  '^/api' : ''
+                }
+            }
+        },
+        hot: true
+    },
+    plugins: [
+        // new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            'process.env.PORT' : JSON.stringify(process.env.PORT),
+        }),
+        new HtmlWebPackPlugin({
+            template: "./public/index.html", 
+            filename: "./index.html",
+            inject: 'body'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    } /*,
+    resolve: {
+        alias: {
+            'react-router-dom': path.resolve('./node_modules/react-router-dom')
+        }
+    } */
+};
