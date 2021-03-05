c8e3f2d5550034008bf81af0b4ee91c7fa1d3cfe
Felipe Tun <felipe.tun.cauich@gmail.com>
2021-03-04 21:08:03 -0500

    Layout

--- fe/index.js
+++ fe/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routers from './Routes';
+import Layout from './Layout';
 
-ReactDOM.render(<Routers />, document.getElementById('root'));
+ReactDOM.render(<Layout><Routers /></Layout>, document.getElementById('root'));
